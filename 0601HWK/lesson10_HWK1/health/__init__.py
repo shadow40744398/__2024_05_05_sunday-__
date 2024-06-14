9c75dad60cae4be288bd1dedf2598855a3f42206
shadow40744398 <excalibur902941@gmail.com>
2024-06-14 14:00:43 +0000

    modity

--- 0601HWK/lesson10_HWK1/health/__init__.py
+++ 0601HWK/lesson10_HWK1/health/__init__.py
@@ -1,24 +1,24 @@
 class BMI():
-    def __init__(self, n, h, w):
+    def __init__(self, n=str, h= float, w= float):
         self.__name = n
         self.__height = h / 100
         self.__weight = w
 
     @property
-    def name(self):
+    def name(self)->str:
         return self.__name
     
     @property
-    def height(self):
+    def height(self)->float:
         return self.__height
     
     @property
-    def weight(self):
+    def weight(self)->float:
         return self.__weight
     
     
     def bmi(self)->float:
-        return round(self.weight / self.height ** 2, 5)
+        return round(self.weight / self.height ** 2, 2)
     
     def status(self)->str:
         if self.bmi() < 18.5:
--- 0601HWK/lesson10_HWK1/index.py
+++ 0601HWK/lesson10_HWK1/index.py
@@ -1,20 +1,18 @@
 import pyinputplus as pyip
 from health import BMI
 
-def main():
-    try:
-        name =  pyip.inputStr("請輸入你的名字:")
+try:
+    def main():
+        
+        name = pyip.inputStr("請輸入你的名字:")
         height = pyip.inputFloat("請輸入你的身高(cm):",min=0,max=250)
         weight = pyip.inputFloat("請輸入你的體重(kg):",min=0,max=200)
 
-        bmi = BMI(height, weight)
-        status = BMI(object)
+        object = BMI(name, height, weight)
 
         print(f"{object.name}, 您的BMI值為 {object.bmi()}")
         print(f"您的體重為 {object.status()}")
-
-    except Exception as e:
-                print(f"EXCEPTION - {type(e)}")
-
+except Exception as e:
+		print(f"EXCEPTION:{type(e)}")
 if __name__ == "__main__":
     main()
