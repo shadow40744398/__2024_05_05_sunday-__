8c08d6f8902ded8345a1896ec8aa0d01f3ad6055
shadow40744398 <excalibur902941@gmail.com>
2024-07-14 07:39:29 +0000

    modity

--- linebot_test/main.py
+++ linebot_test/main.py
@@ -1,19 +1,37 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask,request, abort
 from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 import os
 load_dotenv()
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
 
 @app.route("/")
 def index():
-    return "<h1>我的第一個網站</h1>"
+    return "<h1>LineBot的webhook的程式</h1>"
 
-@app.route("/pwd")
-def pwd():
-    return f'''
-    <h2>{os.environ['CHANNEL_ACCESS_TOKEN']}</h2>
-    <h2>{os.environ['CHANNEL_SECRET']}</h2>
-    <h2>{os.environ['Gemini_API_KEY']}</h2>
-    '''
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'OK'
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    genai.configure(api_key=os.environ['Gemini_API_KEY'])
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(event.message.text)
+    message = TextSendMessage(text=response.text)
+    line_bot_api.reply_message(event.reply_token,message)
